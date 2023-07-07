82fda0b97736fbbbf01ee8a5b82e12590e818747
Singh <sabhisheksignh343104@gmail.com>
2023-07-07 20:31:05 +0530

    main.py file added

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+options = '''
+[1]     Feelings of Emoticons
+[2]     Fun Facts
+[3]     Python jokes
+[4]     Random jokes
+[5]     Create Fake Identity
+[6]     Exit
+'''
+
+print(f"\n\t\t****************", f"{Fore.YELLOW}Fun with Python", "****************\n")
+
+while(True):
+    print(options)
+    usr = input("Enter option: ")
+    if usr == "1":
+        pass
